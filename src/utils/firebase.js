b28e031ed8829981c49fc002265d0a7e821d62bb
Prabhdeep Singh <26prabhdeep@gmail.com>
2026-03-23 20:41:03 +0530

    minor change

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyAY6KQYIAunyuT1NlbBJaSPB-bh-2l0CP0",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
 	authDomain: "netflixgpt-b601c.firebaseapp.com",
 	projectId: "netflixgpt-b601c",
 	storageBucket: "netflixgpt-b601c.firebasestorage.app",
